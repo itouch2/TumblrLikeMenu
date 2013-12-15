f4f4110ec7ab2117c6a3366e219d0820011e67da
Tu You <yoututouch@gmail.com>
2013-12-16 01:06:43 +0800

    Add files finally.

---
+++ TumblrLikeMenu/TumblrLikeMenu/TumblrLikeMenu.h
@@ -0,0 +1,13 @@
+//
+//  TumblrLikeMenu.h
+//  TumblrLikeMenu
+//
+//  Created by Tu You on 12/16/13.
+//  Copyright (c) 2013 Tu You. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TumblrLikeMenu : UIView
+
+@end
---
+++ TumblrLikeMenu/TumblrLikeMenu/TumblrLikeMenu.m
@@ -0,0 +1,31 @@
+//
+//  TumblrLikeMenu.m
+//  TumblrLikeMenu
+//
+//  Created by Tu You on 12/16/13.
+//  Copyright (c) 2013 Tu You. All rights reserved.
+//
+
+#import "TumblrLikeMenu.h"
+
+@implementation TumblrLikeMenu
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
