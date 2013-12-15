1d3413492111333ba6e912648b135e44d7678d8e
Tu You <yoututouch@gmail.com>
2013-12-16 01:02:32 +0800

    Delete folder

--- TumblrLikeMenu/ViewController.m
+++ TumblrLikeMenu/ViewController.m
@@ -23,7 +23,7 @@
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    // Dispose of any resourcesadfsfs that can be recreated.
 }
 
 @end
